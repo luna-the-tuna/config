30434f639ddbbd6eed7101a921b0d142ceb95b91
Luna Heyman <contact@luna.fish>
2026-06-21 14:16:49 +0200

    feat(hosts/crona): add steam

--- hosts/crona/configuration.nix
+++ hosts/crona/configuration.nix
@@ -43,6 +43,13 @@
     xwayland.enable = false;
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
   services.pipewire = {
     enable = true;
     pulse.enable = true;
