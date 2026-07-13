ed9b9f1dbe6fb7f8dab0995a7e961b2947dac2b0
Luna Heyman <contact@luna.fish>
2026-07-13 10:25:00 +0200

    build(flake): initialize nix flake with flake-parts

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "My Linux and MacOS system configurations in a Nix flake";
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } ./nix/flake;
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    systems = {
+      url = "github:nix-systems/default";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+}
