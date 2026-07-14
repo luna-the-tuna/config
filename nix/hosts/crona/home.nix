11fd3cc230bcceb91fb1465a9f74c0cf707f1ab2
Luna Heyman <contact@luna.fish>
2026-07-14 10:38:24 +0200

    feat(hosts/crona): add xdg user directories

--- nix/hosts/crona/home.nix
+++ nix/hosts/crona/home.nix
@@ -1,4 +1,7 @@
 { config, ... }:
+let
+  inherit (config.home) homeDirectory;
+in
 {
   home = {
     stateVersion = config.home.version.release;
@@ -7,5 +10,20 @@
 
   programs = {
     home-manager.enable = true;
+  };
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+
+    desktop = "${homeDirectory}/desktop";
+    documents = "${homeDirectory}/documents";
+    download = "${homeDirectory}/downloads";
+    music = "${homeDirectory}/music";
+    pictures = "${homeDirectory}/pictures";
+    projects = "${homeDirectory}/projects";
+    publicShare = "${homeDirectory}/public";
+    templates = "${homeDirectory}/templates";
+    videos = "${homeDirectory}/videos";
   };
 }
