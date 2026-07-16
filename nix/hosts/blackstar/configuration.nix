e5e1146ffce11da20f0f16421980592d2e2cf88e
Luna Heyman <contact@luna.fish>
2026-07-16 19:20:41 +0200

    feat(modules/blackstar): add jellyfin host to nginx configuration

--- nix/hosts/blackstar/configuration.nix
+++ nix/hosts/blackstar/configuration.nix
@@ -82,5 +82,22 @@
         proxyWebsockets = true;
       };
     };
+
+    virtualHosts."jellyfin.luna.fish" = {
+      enableACME = true;
+      forceSSL = true;
+
+      locations."/" = {
+        proxyPass = "http://10.0.0.2:8096";
+        proxyWebsockets = true;
+
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+    };
   };
 }
