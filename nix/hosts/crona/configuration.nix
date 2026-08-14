58c98ef7189e436abb2b71f79180e081d941c1bd
Luna Heyman <contact@luna.fish>
2026-08-14 14:24:49 +0200

    refactor(hosts/crona): use syncplay without overriding source

--- nix/hosts/crona/configuration.nix
+++ nix/hosts/crona/configuration.nix
@@ -31,15 +31,7 @@
     pkgs.gimp
     pkgs.kdePackages.kdenlive
     pkgs.qbittorrent
-
-    (pkgs.syncplay.overrideAttrs {
-      src = pkgs.fetchFromGitHub {
-        repo = "syncplay";
-        owner = "Syncplay";
-        rev = "578b7b9ea25b505a645219cc7e4b755b728b8efe";
-        hash = "sha256-1GM3KTwC7kngtsdMCLRepubkOyuM7bXDpWoQTeuzB3M=";
-      };
-    })
+    pkgs.syncplay
   ];
 
   boot.initrd.availableKernelModules = [
